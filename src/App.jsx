165981b07bb2c619060c683e0a510a80dd0f61a4
abhay1609 <abhaypratap9848@gmail.com>
2024-08-19 21:11:25 +0530

    CRUD

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { useEffect, useMemo, useRef, useState } from "react";
 import './App.css';
 
 function App() {
+  const [updateData,setUpdateDate]=useState({id:0,name:'',description:'',interests:''})
  
 
   const [error,setError]=useState('')
@@ -17,16 +18,32 @@ function App() {
       <div className="errorMessage">
       {error.length==0?<div></div>:<div>{error}</div>}</div>
       <div className="inputForm">
-       <InputCard  setError={setError}></InputCard>
+       <InputCard  setError={setError} updateData={updateData} setUpdateDate={setUpdateDate}></InputCard>
        </div>
 
 
-    <CardWrapper ></CardWrapper>
+    <CardWrapper setUpdateDate={setUpdateDate} ></CardWrapper>
 
     </div>
   )
 }
-function CardData({name,description,interests}){
+function CardData({id,name,description,interests,setUpdateDate}){
+  async function deleteCard(id){
+
+    let response=await fetch("http://localhost:3000/"+id,{method:"DELETE"})
+    let data=await response.json()
+    console.log(data)
+    
+    
+  }
+  async function updateCard(id,name,description,interests){
+    setUpdateDate({id,name,description,interests})
+    
+
+
+    
+
+  }
   return (
     <div style={{width:"100%",padding:"10px", border:"solid 1px black"}}>
     <h1>{name}</h1>
@@ -43,12 +60,14 @@ function CardData({name,description,interests}){
     </ul>
     <button style={{backgroundColor:"blue",color:"white", padding:"3px", margin:"10px"}}>LinkedIn</button>
     <button>Twitter</button>
+    <button onClick={()=>deleteCard(id)}>Delete</button>
+    <button onClick={()=>updateCard(id,name,description,interests)}>Update</button>
     
   </div>
 
   )
 }
-function CardWrapper(){
+function CardWrapper({setUpdateDate}){
   const [data,setData]=useState([])
   function getData(){
     setInterval(()=>fetch("http://localhost:3000/").then(
@@ -71,7 +90,7 @@ function CardWrapper(){
     <div>
     {data.map((item)=>{
         return(
-          <CardData name={item.name} description={item.name} interests={item.interests}></CardData>
+          <CardData key={item.id} setUpdateDate={setUpdateDate} id={item.id} name={item.name} description={item.description} interests={item.interests}></CardData>
         )
       })
     }
@@ -88,8 +107,25 @@ function InputCard(props){
   const descRef=useRef("");
   const interestRef=useRef('')
   function DisplayCard(e){
-    e.preventDefault();
+    if(props.updateData.id!=0){
+
+      const currentData={name:nameRef.current.value,description:descRef.current.value,interests:[interestRef.current.value]}
+      fetch("http://localhost:3000/"+props.updateData.id,{
+        method:"PUT",
+        headers:{
+          'Content-Type':'application/json'
+        },
+        body:JSON.stringify(currentData)}).then(
+         async (response)=>{
+         let  res=await response.json();
+         console.log(res)
+         })
+         return props.setUpdateDate({id:0,name:'',description:'',interests:''})
+
+    }else{
+
     console.log("hangle")
+ 
     const currentData={name:nameRef.current.value,description:descRef.current.value,interests:[interestRef.current.value]}
     fetch("http://localhost:3000/",{
       method:"POST",
@@ -105,9 +141,23 @@ function InputCard(props){
       }else{
         props.setError(data.msg)
       }
+    }
       
-      
-})}
+)}}
+function changeName(e){
+console.log(props.updateData.id)
+
+  if(props.updateData.id==0){
+    nameRef.current.value=e.target.value
+    
+
+  }else{
+    nameRef.current.value=props.updateData.name
+    
+
+
+  }
+}
 
 
 
@@ -115,9 +165,9 @@ function InputCard(props){
   return (
     <div className="formContainer">
     <form onSubmit={(e)=>DisplayCard(e)} className="form">
-      <input placeholder="Name" type="text" name="name"  ref={nameRef} className="inputField"></input>
-      <input placeholder="Description" name="name"  ref={descRef} type="text" className="inputField"></input>
-      <input placeholder="Interest" name="name"  ref={interestRef} type="text" className="inputField"></input>
+      <input type="text" name="name" defaultValue={props.updateData.name} onChange={(e)=>changeName(e)} placeholder="Name" ref={nameRef} className="inputField"></input>
+      <input placeholder="Description" defaultValue={props.updateData.description} ref={descRef} type="text" className="inputField"></input>
+      <input placeholder="Interest" name="name" defaultValue={props.updateData.interests} ref={interestRef} type="text" className="inputField"></input>
       <div className="buttonContainer">
       <input type="submit" className="submitButton"></input></div>
       </form>
